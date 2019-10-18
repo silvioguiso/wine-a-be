c43d85e92bffe20dcd86389fab4ca17811d565f6
silvioguiso <silvio.guiso@gmail.com>
2019-10-17 17:25:37 -0700

    Added main screen with 2.5 second timeout to show winery list.

---
+++ app/src/main/java/com/example/wineabe/ListWineriesActivity.java
@@ -0,0 +1,14 @@
+package com.example.wineabe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListWineriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_wineries);
+    }
+}
--- app/src/main/java/com/example/wineabe/MainActivity.java
+++ app/src/main/java/com/example/wineabe/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.wineabe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread timer= new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    //Display for 2.5 seconds
+                    sleep(2500);
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    Intent openList = new Intent(getApplicationContext(), ListWineriesActivity.class);
+                    startActivity(openList);
+                }
+            }
+        };
+
+        timer.start();
+    }
+
+    //We don't need this one anymore, so we destroy it as soon as it's out of sight.
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
