582ce9d59c6e11bf50f3fcadc8b082f436cbad5e
silvioguiso <silvio.guiso@gmail.com>
2019-10-24 18:36:15 -0700

    Changed recyclerview layoutmanager.

--- app/src/main/java/com/example/wineabe/MainActivity/WineriesListFragment.java
+++ app/src/main/java/com/example/wineabe/MainActivity/WineriesListFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -23,7 +24,7 @@ public class WineriesListFragment extends Fragment {
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1, GridLayoutManager.VERTICAL, false));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
 
         recyclerView.setAdapter(new WineryCardRecyclerViewAdapter());
 
